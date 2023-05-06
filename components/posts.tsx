a92b90e54e43d756c3d05d62c540597d3a7af310
nogtk <taka.naoga@moneyforward.co.jp>
2023-05-06 15:45:59 +0900

    Remove Author component

--- components/posts.tsx
+++ components/posts.tsx
@@ -8,8 +8,8 @@ type Props = {
 const Posts = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+      <h2 className="mt-4 mb-4 text-5xl md:text-6xl font-bold tracking-tighter leading-tight">
+        Posts
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
@@ -18,7 +18,6 @@ const Posts = ({ posts }: Props) => {
             title={post.title}
             coverImage={post.coverImage}
             date={post.date}
-            author={post.author}
             slug={post.slug}
             excerpt={post.excerpt}
           />
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,6 @@ export const getStaticProps = async () => {
     'title',
     'date',
     'slug',
-    'author',
     'coverImage',
     'excerpt',
   ])
