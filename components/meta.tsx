fd8962aaa289aab78cdddebffb72dfbc9c9954f0
nogtk <taka.naoga@moneyforward.co.jp>
2023-06-10 17:47:35 +0900

    Update favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -11,15 +11,13 @@ const Meta = () => {
       />
       <link
         rel="icon"
-        type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👕</text></svg>"
       />
       <link
         rel="icon"
-        type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👕</text></svg>"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
