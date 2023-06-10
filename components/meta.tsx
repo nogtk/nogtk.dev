a09c7c7b0703aab8760ec6401fd09f86ca3b29cd
nogtk <taka.naoga@moneyforward.co.jp>
2023-06-10 18:22:50 +0900

    Slim up meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,14 +1,9 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import { CMS_NAME } from '../lib/constants'
 
 const Meta = () => {
   return (
     <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
       <link
         rel="icon"
         sizes="32x32"
@@ -19,17 +14,11 @@ const Meta = () => {
         sizes="16x16"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👕</text></svg>"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
+        rel="icon alternate"
+        type="image/png"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f455.png"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
