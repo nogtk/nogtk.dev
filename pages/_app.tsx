c1dac172e74b78976d14e79355c53d798f6333a9
nogtk <taka.naoga@moneyforward.co.jp>
2024-01-23 23:12:08 +0900

    summary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app'
-import { useEffect } from 'react';
-import '../styles/index.css'
-import 'zenn-content-css';
-import { DefaultSeo } from 'next-seo';
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import "../styles/index.css";
+import "zenn-content-css";
+import { DefaultSeo } from "next-seo";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -11,26 +11,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo
-        titleTemplate = '%s | nogtk.dev'
-        defaultTitle = 'nogtk.dev'
-        description = "Tech insights, with a sprinkle of life's adventures"
-        twitter = {{
-          cardType: 'summary_large_image',
-          site: 'nogtk.dev',
-          handle: '@_nogtk_',
+        titleTemplate="%s | nogtk.dev"
+        defaultTitle="nogtk.dev"
+        description="Tech insights, with a sprinkle of life's adventures"
+        twitter={{
+          cardType: "summary",
+          site: "nogtk.dev",
+          handle: "@_nogtk_",
         }}
-        openGraph = {{
-          type: 'website',
-          url: 'https://nogtk.dev',
-          title: 'nogtk.dev',
+        openGraph={{
+          type: "website",
+          url: "https://nogtk.dev",
+          title: "nogtk.dev",
           description: "Tech insights, with a sprinkle of life's adventures",
-          locale: 'ja_JP',
-          siteName: 'nogtk.dev',
+          locale: "ja_JP",
+          siteName: "nogtk.dev",
           images: [
             {
-              url: 'https://res.cloudinary.com/di1lterwq/image/upload/v1692798736/Frame_1_pt3mzq.png'
-            }
-          ]
+              url: "https://res.cloudinary.com/di1lterwq/image/upload/v1692798736/Frame_1_pt3mzq.png",
+            },
+          ],
         }}
       />
       <Component {...pageProps} />
