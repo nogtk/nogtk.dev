8455bc574cbc44a478f8595302bc9470da1f5700
nogtk <taka.naoga@moneyforward.co.jp>
2024-08-24 20:50:44 +0900

    Adjust 1024px

--- components/post/post-container.tsx
+++ components/post/post-container.tsx
@@ -13,13 +13,13 @@ const PostContainer = ({ post }: Props) => {
   return (
     <>
       <article className="flex-grow mb-32">
-        <div className="flex flex-col xl:flex-row justify-center lg:mx-32">
+        <div className="flex flex-col xl:flex-row justify-center xl:mx-32">
           <Head>
             <Script src="https://embed.zenn.studio/js/listen-embed-event.js" />
           </Head>
-          <main className="order-2 lg:order-1">
+          <main className="order-2 xl:order-1">
             {/* 1024px の時は max-w-xl がちょうどいいが、今はそれを無視しておく */}
-            <div className="w-full lg:max-w-4xl mx-auto py-6 sm:px-6 lg:px-10">
+            <div className="w-full xl:max-w-4xl mx-auto py-6 sm:px-6 lg:px-10">
               <div className="px-4 py-6 sm:px-0">
                 <div className="rounded-lg bg-white shadow-lg overflow-hidden">
                   <div className="p-4 sm:p-6 lg:p-8">
@@ -39,8 +39,8 @@ const PostContainer = ({ post }: Props) => {
               </div>
             </div>
           </main>
-          <aside className="w-full lg:w-1/3 order-1 lg:order-2">
-            <div className="sticky top-12 mt-12 lg:mt-0 mx-4 lg:mx-0 xl:mt-12">
+          <aside className="w-full xl:w-1/3 order-1 xl:order-2 lg:mx-auto">
+            <div className="sticky top-12 xl:mt-12 mx-4 xl:mx-0 xl:mt-12">
               <TableOfContents />
             </div>
           </aside>
